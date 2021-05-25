f23438046fb6df8653c03eeb1d897b9701723285
Thobani <gtmwandla@gmail.com>
2021-05-25 23:40:11 +0200

    commented some un-necessary code

--- model.py
+++ model.py
@@ -65,18 +65,18 @@ def _preprocess_data(data):
     predict_vector = feature_vector_df[['Total_Qty_Sold','Stock_On_Hand']]
 
     # converting date column into proper date formart
-    feature_vector_df.date = pd.to_datetime(feature_vector_df.Date) 
-    feature_vector_df["Quarter"] = feature_vector_df.date.dt.quarter 
-    feature_vector_df["Month"] = feature_vector_df.date.dt.month
-    feature_vector_df["Year"] = feature_vector_df.date.dt.year
+    # feature_vector_df.date = pd.to_datetime(feature_vector_df.Date) 
+    # feature_vector_df["Quarter"] = feature_vector_df.date.dt.quarter 
+    # feature_vector_df["Month"] = feature_vector_df.date.dt.month
+    # feature_vector_df["Year"] = feature_vector_df.date.dt.year
 
-    # Don't need the date calumn anymore
-    feature_vector_df = feature_vector_df.drop('Date', axis=1)
+    # # Don't need the date calumn anymore
+    # feature_vector_df = feature_vector_df.drop('Date', axis=1)
 
-    # Dummy variables
-    dummy_df = pd.get_dummies(feature_vector_df, drop_first=True)
+    # # Dummy variables
+    # dummy_df = pd.get_dummies(feature_vector_df, drop_first=True)
 
-    return dummy_df
+    return predict_vector
 
 def load_model(path_to_model:str):
     """Adapter function to load our pretrained model into memory.
