3e30f5a55e973114bb37f96e35982855a6a7f63a
Thobani <gtmwandla@gmail.com>
2021-05-26 11:13:21 +0200

    using default set-up

--- model.py
+++ model.py
@@ -61,21 +61,21 @@ def _preprocess_data(data):
     # ----------- Replace this code with your own preprocessing steps --------
     # adding three dummy varibles from date column
     feature_vector_df = feature_vector_df[(feature_vector_df['Commodities'] == 'APPLE GOLDEN DELICIOUS')]
-    # predict_vector = feature_vector_df[['Total_Qty_Sold','Stock_On_Hand']]
+    predict_vector = feature_vector_df[['Total_Qty_Sold','Stock_On_Hand']]
 
     # converting date column into proper date formart
-    feature_vector_df["date"] = pd.to_datetime(feature_vector_df["Date"]) 
-    feature_vector_df["Quarter"] = feature_vector_df["date"].dt.quarter 
-    feature_vector_df["Month"] = feature_vector_df["date"].dt.month
-    feature_vector_df["Year"] = feature_vector_df["date"].dt.year    
+    # feature_vector_df["date"] = pd.to_datetime(feature_vector_df["Date"]) 
+    # feature_vector_df["Quarter"] = feature_vector_df["date"].dt.quarter 
+    # feature_vector_df["Month"] = feature_vector_df["date"].dt.month
+    # feature_vector_df["Year"] = feature_vector_df["date"].dt.year    
 
-    # Don't need the date calumn anymore
-    feature_vector_df = feature_vector_df.drop(['date', 'Date'], axis=1)
+    # # Don't need the date calumn anymore
+    # feature_vector_df = feature_vector_df.drop(['date', 'Date'], axis=1)
 
-    # Dummy variables
-    dummy_df = pd.get_dummies(feature_vector_df, drop_first=True)
+    # # Dummy variables
+    # dummy_df = pd.get_dummies(feature_vector_df, drop_first=True)
 
-    return dummy_df
+    return predict_vector
 
 def load_model(path_to_model:str):
     """Adapter function to load our pretrained model into memory.
